40649e319ef67e6805f71c2572874727eec10a7f
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-06-09 23:43:23 -0300

    update desafio 23 slide show

---
+++ desafio23-form-multi-step/script.js
@@ -0,0 +1,37 @@
+const list = document.querySelectorAll(".step")
+
+function currentIndex(listItems) {
+  for (let [item, val] of listItems.entries()) {
+    if (val.classList.contains('show')) {
+      var current = item
+    }
+  }
+  return current
+}
+
+function next() {
+  current = currentIndex(list)
+  limit = list.length - 1
+  if (current == limit) {
+    var next = 0
+    list[current].classList.remove('show')
+    list[next].classList.add('show')
+  } else {
+    list[current].classList.remove('show')
+    var next = current + 1
+    list[next].classList.add('show')
+  }
+}
+
+function previous() {
+  current = currentIndex(list)
+  if (current == 0) {
+    var next = list.length - 1
+    list[current].classList.remove('show')
+    list[next].classList.add('show')
+  } else {
+    list[current].classList.remove('show')
+    var next = current - 1
+    list[next].classList.add('show')
+  }
+}
