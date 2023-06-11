a2742d4cddba26057b7efd9b7c50c559233a80dd
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-06-10 21:59:43 -0300

    update desafio 23 header done icon

--- desafio23-form-multi-step/script.js
+++ desafio23-form-multi-step/script.js
@@ -1,39 +1,45 @@
 const listSteps = document.querySelectorAll(".step")
 const listHeader = document.querySelectorAll(".formStep")
+const checkIcon = '<i class="ph-bold ph-check"></i>'
 
 function currentIndex(listItems) {
   for (let [item, val] of listItems.entries()) {
     if (val.classList.contains('show')) {
-      var current = item
+      return item
     }
   }
-  return current
 }
 
-function addRemoveClass(index, current, next, className) {
-  index[current].classList.remove(className)
-  index[next].classList.add(className)
+function addRemoveClass(array, current, next, className) {
+  array[current].classList.remove(className)
+  array[next].classList.add(className)
+}
+
+function addRemoveCheckIcon(current, action) {
+  if (action == 'add') {
+    listHeader[current].classList.add('done')
+    listHeader[current].querySelector('span').innerHTML = checkIcon
+  } else {
+    listHeader[current - 1].classList.remove('done')
+    listHeader[current - 1].querySelector('span').innerHTML = current
+  }
 }
 
 function next() {
   var current = currentIndex(listSteps)
   limit = listSteps.length - 1
-  if (current == limit) {
-    addRemoveClass(listSteps, current, 0, 'show')
-    addRemoveClass(listHeader, current, 0, 'active')
-  } else {
+  if (current != limit) {
     addRemoveClass(listSteps, current, current + 1, 'show')
     addRemoveClass(listHeader, current, current + 1, 'active')
+    addRemoveCheckIcon(current, 'add')
   }
 }
 
 function previous() {
   var current = currentIndex(listSteps)
-  if (current == 0) {
-    addRemoveClass(listSteps, current, listSteps.length - 1, 'show')
-    addRemoveClass(listHeader, current, listSteps.length - 1, 'active')
-  } else {
+  if (current != 0) {
     addRemoveClass(listSteps, current, current - 1, 'show')
     addRemoveClass(listHeader, current, current - 1, 'active')
+    addRemoveCheckIcon(current, 'remove')
   }
 }
