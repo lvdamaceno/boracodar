00aa128f44ebda318ebb998bf12c0726d33a5c32
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-06-14 18:36:43 -0300

    init desafio 24

---
+++ desafio24-color-light/script.js
@@ -0,0 +1,42 @@
+const colorDisplay = document.querySelector('.colorDisplay')
+const colorThumb = document.getElementsByClassName('.slider.hue::-webkit-slider-thumb')
+
+var sliderHue = document.getElementById("slider hue")
+var sliderLight = document.getElementById("slider light");
+var sliderSaturation = document.getElementById("slider saturation")
+
+sliderHue.onmousemove = function () {
+  var output = document.getElementById("hueValue");
+  var sliderHueValue = sliderHue.value
+  var sliderLightValue = sliderLight.value
+  var sliderSaturationValue = sliderSaturation.value
+  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  colorDisplay.style.backgroundColor = hueColor;
+  colorDisplay.style.boxShadow = shadowColor
+  output.innerHTML = sliderHueValue;
+}
+
+sliderLight.onmousemove = function () {
+  var output = document.getElementById("lightValue");
+  var sliderHueValue = sliderHue.value
+  var sliderLightValue = sliderLight.value
+  var sliderSaturationValue = sliderSaturation.value
+  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  colorDisplay.style.backgroundColor = hueColor;
+  colorDisplay.style.boxShadow = shadowColor
+  output.innerHTML = sliderLightValue;
+}
+
+sliderSaturation.onmousemove = function () {
+  var output = document.getElementById("lightSaturation");
+  var sliderHueValue = sliderHue.value
+  var sliderLightValue = sliderLight.value
+  var sliderSaturationValue = sliderSaturation.value
+  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
+  colorDisplay.style.backgroundColor = hueColor;
+  colorDisplay.style.boxShadow = shadowColor
+  output.innerHTML = sliderSaturationValue;
+}
