0df189459a45d9261c156fa0611e8ec89c48cbaf
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-06-14 23:19:05 -0300

    update desafio 24 show hsl, rgb and hex values

--- desafio24-color-light/script.js
+++ desafio24-color-light/script.js
@@ -1,41 +1,58 @@
-const colorDisplay = document.querySelector('.colorDisplay')
+const colorDisplayClass = document.querySelector('.colorDisplay')
 
-var sliderHue = document.getElementById("slider hue")
-var sliderLight = document.getElementById("slider light");
-var sliderSaturation = document.getElementById("slider saturation")
+var Hue = document.getElementById("slider hue")
+var Saturation = document.getElementById("slider saturation")
+var Light = document.getElementById("slider light");
 
-sliderHue.onmousemove = function () {
-  var output = document.getElementById("hueValue");
-  var sliderHueValue = sliderHue.value
-  var sliderLightValue = sliderLight.value
-  var sliderSaturationValue = sliderSaturation.value
-  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  colorDisplay.style.backgroundColor = hueColor;
-  colorDisplay.style.boxShadow = shadowColor
-  output.innerHTML = sliderHueValue + ',';
+function convertHslToRgb(h, s, l) {
+  s /= 100;
+  l /= 100;
+  const k = n => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = n =>
+    l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  var r = 255 * f(0)
+  var g = 255 * f(8)
+  var b = 255 * f(4)
+  return [Math.trunc(r), Math.trunc(g), Math.trunc(b)]
 }
 
-sliderLight.onmousemove = function () {
-  var output = document.getElementById("lightValue");
-  var sliderHueValue = sliderHue.value
-  var sliderLightValue = sliderLight.value
-  var sliderSaturationValue = sliderSaturation.value
-  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  colorDisplay.style.backgroundColor = hueColor;
-  colorDisplay.style.boxShadow = shadowColor
-  output.innerHTML = sliderLightValue + '%,';
+function convertRgbToHex(r, g, b) {
+  return Math.abs(r).toString(16) + Math.abs(g).toString(16) + Math.abs(b).toString(16);
 }
 
-sliderSaturation.onmousemove = function () {
-  var output = document.getElementById("lightSaturation");
-  var sliderHueValue = sliderHue.value
-  var sliderLightValue = sliderLight.value
-  var sliderSaturationValue = sliderSaturation.value
-  var hueColor = `hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  var shadowColor = `0px 0px 53px 13px hsl(${sliderHueValue},${sliderLightValue}%,${sliderSaturationValue}%)`
-  colorDisplay.style.backgroundColor = hueColor;
-  colorDisplay.style.boxShadow = shadowColor
-  output.innerHTML = sliderSaturationValue + '%)';
+function displayColorChange() {
+  var hueColor = `hsl(${Hue.value},${Saturation.value}%,${Light.value}%)`
+  var shadowColor = `0px 0px 53px 13px ${hueColor}`
+  colorDisplayClass.style.backgroundColor = hueColor;
+  colorDisplayClass.style.boxShadow = shadowColor
+  showHslValues("hueValue", "saturationValue", "lightValue")
+  showRgbValues("rValue", "gValue", "bValue")
+  showHexValues("hexValue")
+}
+
+function showHslValues(hue, saturation, light) {
+  var hueNumber = document.getElementById(hue);
+  var saturationNumber = document.getElementById(saturation);
+  var lightNumber = document.getElementById(light);
+  hueNumber.innerHTML = Hue.value + ',';
+  saturationNumber.innerHTML = Saturation.value + '%,';
+  lightNumber.innerHTML = Light.value + '%)';
+}
+
+
+function showRgbValues(red, green, blue) {
+  var rgb = convertHslToRgb(Hue.value, Saturation.value, Light.value)
+  var redNumber = document.getElementById(red);
+  var greenNumber = document.getElementById(green);
+  var blueNumber = document.getElementById(blue);
+  redNumber.innerHTML = rgb[0];
+  greenNumber.innerHTML = rgb[1];
+  blueNumber.innerHTML = rgb[2];
+}
+
+function showHexValues(hex) {
+  var rgb = convertHslToRgb(Hue.value, Saturation.value, Light.value)
+  var hexNumber = document.getElementById(hex);
+  hexNumber.innerHTML = convertRgbToHex(rgb[0], rgb[1], rgb[2]);
 }
